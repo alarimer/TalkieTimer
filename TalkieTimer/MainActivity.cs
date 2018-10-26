fdd87010c25f58e12c0fb3f78fe59ec36ad1371a
Alan Larimer <alan.larimer@outlook.com>
2018-10-26 00:25:18 -0400

    Two person tracking

--- TalkieTimer/MainActivity.cs
+++ TalkieTimer/MainActivity.cs
@@ -11,9 +11,47 @@ namespace TalkieTimer
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            long elapsed1 = 0;
+            long elapsed2 = 0;
+
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.MainActivity);
+
+            // controls
+            Switch s1 = FindViewById<Switch>(Resource.Id.Person1Switch);
+            Chronometer c1 = FindViewById<Chronometer>(Resource.Id.Person1Chronometer);
+
+            s1.CheckedChange += (sender, e) =>
+            {
+                if (e.IsChecked)
+                {
+                    c1.Base = SystemClock.ElapsedRealtime() - elapsed1;
+                    c1.Start();
+                }
+                else
+                {
+                    c1.Stop();
+                    elapsed1 = SystemClock.ElapsedRealtime() - c1.Base;
+                }
+            };
+
+            Switch s2 = FindViewById<Switch>(Resource.Id.Person2Switch);
+            Chronometer c2 = FindViewById<Chronometer>(Resource.Id.Person2Chronometer);
+
+            s2.CheckedChange += (sender, e) =>
+            {
+                if (e.IsChecked)
+                {
+                    c2.Base = SystemClock.ElapsedRealtime() - elapsed2;
+                    c2.Start();
+                }
+                else
+                {
+                    c2.Stop();
+                    elapsed2 = SystemClock.ElapsedRealtime() - c2.Base;
+                }
+            };
         }
     }
 }
