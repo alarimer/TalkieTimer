a3459e56a1ca55d8b4c02e5daf3394f497798fda
Alan Larimer <alan.larimer@outlook.com>
2018-10-25 21:36:37 -0400

    Rename main activity

--- TalkieTimer/MainActivity.cs
+++ TalkieTimer/MainActivity.cs
@@ -13,7 +13,7 @@ namespace TalkieTimer
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.MainActivity);
         }
     }
 }
